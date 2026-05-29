411fd6d0ba3c38aa5db3e645660f44620eeeecfa
Alvin Chino <chino.9084381@gmail.com>
2026-05-29 19:16:33 -0400

    W03

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    // Constructor
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    // Constructor
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+        return false;
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
