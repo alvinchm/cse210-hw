4c200001fddc0b6253271d7a65180f96eb68c986
Alvin Chino <chino.9084381@gmail.com>
2026-06-13 11:12:08 -0400

    W06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        Circle s3 = new Circle("Green", 6);
+
+
+        shapes.Add(s1);
+        shapes.Add(s2);
+        shapes.Add(s3);
+
+        Console.WriteLine("--- SHAPE AREAS CALCULATOR ---");
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+
+            Console.WriteLine($"The {color} shape has an area of {area:0.00}.");
+        }
     }
 }
