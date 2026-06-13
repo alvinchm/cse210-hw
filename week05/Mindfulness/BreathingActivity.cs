b2efdce0ed568c236f4d07e71f363f925c52af4b
Alvin Chino <chino.9084381@gmail.com>
2026-06-13 10:21:10 -0400

    W05

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+    {
+        _name = "Breathing Activity";
+        _description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < futureTime)
+        {
+            Console.Write("\nBreathe in... ");
+            ShowCountDown(4);
+            Console.Write("\nNow breathe out... ");
+            ShowCountDown(6);
+            Console.WriteLine();
+        }
+
+        DisplayEndingMessage();
+    }
+}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
+//I added a "Session Tracker" feature.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        int activityCount = 0;
+        string choice = "";
+
+        while (choice != "4")
+        {
+            Console.Clear();
+            Console.WriteLine($"--- Mindfulness Program ---");
+            Console.WriteLine($"Activities completed this session: {activityCount}\n");
+
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                BreathingActivity breathing = new BreathingActivity();
+                breathing.Run();
+                activityCount++;
+            }
+            else if (choice == "2")
+            {
+                ReflectingActivity reflecting = new ReflectingActivity();
+                reflecting.Run();
+                activityCount++;
+            }
+            else if (choice == "3")
+            {
+                ListingActivity listing = new ListingActivity();
+                listing.Run();
+                activityCount++;
+            }
+        }
+
+        Console.WriteLine("\nThank you for using the Mindfulness Program. Have a great day!");
     }
 }
