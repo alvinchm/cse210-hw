779844e00445200fc7965e8714b06e1ba8f1a7d7
Alvin Chino <chino.9084381@gmail.com>
2026-06-18 16:22:57 -0400

    W07

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running("03 Nov 2022", 30, 4.8);
+        Cycling cycling = new Cycling("04 Nov 2022", 45, 20.0);
+        Swimming swimming = new Swimming("05 Nov 2022", 20, 15);
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        Console.WriteLine("Exercise Tracking Summaries:\n");
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
